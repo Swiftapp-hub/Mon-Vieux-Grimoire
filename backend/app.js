d0df434f9a8b209a2b82087bbf58e87e69e9205e
SwiftApp <swiftyassistant.io@gmail.com>
2023-07-10 14:27:19 +0200

    Security update

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ require("dotenv").config();
 
 const userRoutes = require('./routes/user');
 const bookRoutes = require('./routes/book');
+const {limiter} = require("./middleware/rate-limiter");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const dbServer = process.env.DB_SERVER;
 const dbUserName = process.env.DB_USER_NAME;
@@ -18,6 +21,8 @@ mongoose.connect(`mongodb+srv://${dbUserName}:${dbPassword}@${dbServer}/?retryWr
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use(helmet);
+
 app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -28,6 +33,8 @@ app.use((req, res, next) => {
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(express.json());
+app.use(mongoSanitize());
+app.use(limiter);
 
 app.use('/api/auth', userRoutes);
 app.use('/api/books', bookRoutes);
